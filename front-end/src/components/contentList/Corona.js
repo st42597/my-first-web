2d8a13b0358f5b7a8b77ce76fe6f2eea17d31936
WillKiss <st42597@gmail.com>
2021-08-24 03:12:04 +0900

    [Fix] Corona Page returnData Output

--- front-end/src/components/contentList/Corona.js
+++ front-end/src/components/contentList/Corona.js
@@ -11,7 +11,7 @@ function Corona() {
   useEffect(() => {
     async function readContent() {
       const ret = await axios.get(URL + 'Corona');
-      setReturnData(ret.data);
+      setReturnData(ret.data[0].accDefRate._text);
       console.log(ret);
     }
     readContent();
@@ -19,7 +19,7 @@ function Corona() {
   return(
     <div>
       공공 데이터 api 활용 연습을 위한 페이지입니다.      
-      {returnData[0].accDefRate._text}
+      {returnData}
     </div>
   );
 }
