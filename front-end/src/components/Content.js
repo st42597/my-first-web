bb2acb4b06ba8575f3353a33b374931701b29545
WillKiss <st42597@gmail.com>
2021-08-26 16:07:43 +0900

    add login modal and board page

--- front-end/src/components/Content.js
+++ front-end/src/components/Content.js
@@ -1,10 +1,10 @@
 import './Content.css';
 import {useState, useEffect} from 'react';
-import axios from 'axios';
 import Home from './contentList/Home';
 import Profile from './contentList/Profile';
 import Github from './contentList/Github';
 import Corona from './contentList/Corona';
+import Board from './contentList/Board';
 
 function Content(props) {
     let styles;
@@ -14,36 +14,24 @@ function Content(props) {
     if(props.sidebarstate === 'ON'){
         styles={width: `calc(70vw - 240px)`};
     }
-    let [returnData, setReturnData] = useState("");
     let URL;
     if(process.env.NODE_ENV === 'production'){
       URL = 'https://willkiss.ml:5000/';
     }else{
       URL = 'http://localhost:5000/';
     }
-    const staticPageList = ['Main', 'Profile'];
-    useEffect(() => {
-      async function readContent() {
-        const ret = await axios.get(URL + props.viewstate);
-          setReturnData(ret.data);
-      }
-      for(let i = 0; i < staticPageList.length; i++){
-        if(props.viewstate === staticPageList[i]){
-          readContent();
-          break;
-        }
-      }
-    }, [props.viewstate])
-    const contentList = [<Home></Home>, <Profile></Profile>, <Github></Github>, <Corona></Corona>];
+    const contentList = [<Home></Home>, <Profile></Profile>, <Board></Board>, <Github></Github>, <Corona></Corona>];
     let contentNumber = 0;
     if(props.viewstate === 'Home')
       contentNumber = 0;
     else if(props.viewstate === 'Profile')
       contentNumber = 1;
-    else if(props.viewstate === 'Github')
+    else if(props.viewstate === 'Board')
       contentNumber = 2;
-    else if(props.viewstate === 'Corona')
+    else if(props.viewstate === 'Github')
       contentNumber = 3;
+    else if(props.viewstate === 'Corona')
+      contentNumber = 4;
     return (
       <div
       className="content-container"
--- front-end/src/components/Sidebar.js
+++ front-end/src/components/Sidebar.js
@@ -1,29 +1,75 @@
 import './Sidebar.css';
+import {useState} from 'react';
+import ReactModal from 'react-modal';
+
+function Guest(props) {
+  let [modalIsOpen, setIsOpen] = useState(false);
+  function submitPW() {
+    let PW = document.getElementById("password").value;
+    if(PW === '1234'){
+      console.log("정답!@");
+      props.setAccount('Admin');
+    }
+  }
+  return(
+    <div className="sidebar-user">
+      <div style={{"margin":"0px 40px 0px 0px"}}>Guest</div>
+      <div onClick={() => setIsOpen(true)}>Login</div>
+      <ReactModal
+      isOpen={modalIsOpen}
+      ariaHideApp={false}
+      shouldCloseOnOverlayClick={true}
+      onRequestClose={() => setIsOpen(false)}
+      >
+        <h1>Admin Password를 입력해주세요.</h1>
+        <input type="text" id="password"></input>
+        <button type="submit" onClick={() => submitPW()}>제출</button>
+      </ReactModal>
+    </div>
+  )
+}
+
+function Admin() {
+  return(
+    <div className="sidebar-user">
+      Admin
+    </div>
+  )
+}
 
 function Sidebar(props) {
-    let styles;
-    if(props.sidebarstate === 'OFF'){
-        styles = {transform: `translateX(-240px)`};
-    }
-    if(props.sidebarstate === 'ON'){
-        styles = {transform: `translateX(0)`};
-    }
-    const clickList = (post) => {
-        props.setViewstate(post);
-    }
-    return (
-        <div
-        className="Side-bar"
-        style={styles}
-        >
-            <ul>
-                <li onClick={() => clickList('Home')}>Home</li>
-                <li onClick={() => clickList('Profile')}>Profile</li>
-                <li onClick={() => clickList('Github')}>Github</li>
-                <li onClick={() => clickList('Corona')}>Corona</li>
-                <li onClick={() => clickList('Description')}>Description</li>
-            </ul>  
-        </div>
+  let [account, setAccount] = useState('Guest');
+  let accountBlock;
+  if(account === 'Guest'){
+    accountBlock = <Guest setAccount={setAccount} />;
+  }else{
+    accountBlock = <Admin />;
+  }
+  let styles;
+  if(props.sidebarstate === 'OFF'){
+    styles = {transform: `translateX(-240px)`};
+  }
+  if(props.sidebarstate === 'ON'){
+    styles = {transform: `translateX(0)`};
+  }
+  const clickList = (post) => {
+    props.setViewstate(post);
+  }
+  return (
+    <div
+    className="sidebar"
+    style={styles}
+    >
+      {accountBlock}
+      <ul>
+        <li onClick={() => clickList('Home')}>Home</li>
+        <li onClick={() => clickList('Profile')}>Profile</li>
+        <li onClick={() => clickList('Github')}>Github</li>
+        <li onClick={() => clickList('Board')}>Board</li>
+        <li onClick={() => clickList('Corona')}>Corona</li>
+        <li onClick={() => clickList('Description')}>Description</li>
+      </ul>  
+    </div>
     );
   }
   
