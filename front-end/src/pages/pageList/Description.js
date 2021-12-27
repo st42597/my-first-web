9fcb83a8164c1291d5054f1666edabfac6d97033
st42597@gmail.com <seonghyeoncho@SeongHyeonui-MacBookPro.local>
2021-12-27 19:01:48 +0900

    fix: <h1> cannot appear as a descendant of <p>.

--- front-end/src/pages/pageList/Description.js
+++ front-end/src/pages/pageList/Description.js
@@ -1,13 +1,13 @@
 function Description() {
   return(
-    <p>
+    <div>
       <h1>my-first-web</h1>
       <div>front-end : Node.js React Library</div>
       <div>back-end : Node.js Express Framework</div>
       <div>Cloud : AWS EC2, ELB</div>
       <div>Client-Front(react)-Back(express)-LB(LoadBalancer)-AWS EC2</div>
       <a href="https://github.com/st42597">GitHub</a>
-    </p>
+    </div>
   )
 }
 export default Description;
