23e677190e90625c2010a7aeff196a4d9119bfb0
st42597 <seonghyeoncho@SeongHyeonui-MacBookPro.local>
2021-12-27 19:04:28 +0900

    test commit

--- front-end/src/pages/pageList/Description.js
+++ front-end/src/pages/pageList/Description.js
@@ -6,7 +6,7 @@ function Description() {
       <div>back-end : Node.js Express Framework</div>
       <div>Cloud : AWS EC2, ELB</div>
       <div>Client-Front(react)-Back(express)-LB(LoadBalancer)-AWS EC2</div>
-      <a href="https://github.com/st42597">GitHub</a>
+      <a href="https://github.com/st42597">GitHub.</a>
     </div>
   )
 }
