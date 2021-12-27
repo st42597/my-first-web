fbfe38c60bf492b715bc951695e05b6d0399ff84
SeongHyeon Cho <seonghyeoncho@SeongHyeonui-MacBookPro.local>
2021-12-27 18:50:09 +0900

    fix: change identation tab to space2

--- front-end/src/pages/pageList/Github.js
+++ front-end/src/pages/pageList/Github.js
@@ -1,20 +1,20 @@
 import {useEffect} from 'react';
 import GitHubCalendar from 'github-calendar';
 function Github() {
-    useEffect(() => {
-        GitHubCalendar(".calendar", "st42597", { responsive: true });
-    })
-    
-    return(
-        <div>
-            <link
-            rel="stylesheet"
-            href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
-            />
-            <div className="calendar">
-            Loading the data just for you.
-            </div>
-        </div>
-    );
+  useEffect(() => {
+  GitHubCalendar(".calendar", "st42597", { responsive: true });
+  })
+
+  return(
+  <div>
+    <link
+    rel="stylesheet"
+    href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"
+    />
+    <div className="calendar">
+      Loading the data just for you.
+    </div>
+  </div>
+  );
 }
 export default Github;
--- front-end/src/pages/pageList/Profile.js
+++ front-end/src/pages/pageList/Profile.js
@@ -2,24 +2,24 @@ import {useEffect, useState} from 'react';
 import axios from 'axios';
 import parse from 'html-react-parser';
 function Profile() {
-    let [returnData, setReturnData] = useState("");
-    let URL;
-    if(process.env.NODE_ENV === 'production'){
-      URL = 'https://willkiss.ml:5000/';
-    }else{
-      URL = 'http://localhost:4000/';
+  let [returnData, setReturnData] = useState("");
+  let URL;
+  if(process.env.NODE_ENV === 'production'){
+    URL = 'https://willkiss.ml:4000/';
+  }else{
+    URL = 'http://localhost:4000/';
+  }
+  useEffect(() => {
+    async function readContent() {
+      const ret = await axios.get(URL + 'Profile');
+        setReturnData(ret.data);
     }
-    useEffect(() => {
-      async function readContent() {
-        const ret = await axios.get(URL + 'Profile');
-          setReturnData(ret.data);
-      }
-      readContent();
-    }, [])
-    return(
-        <div>
-            {parse(returnData)}
-        </div>
-    );
+    readContent();
+  }, [])
+  return(
+    <div>
+      {parse(returnData)}
+    </div>
+  );
 }
 export default Profile;
