fa6d437f0959d02e9cfe93b5158cab8bc906f4f5
st42597 <st42597@gmail.com>
2022-02-17 11:47:02 +0900

    fix: nginx & api caller

--- front-end/src/pages/pageList/Profile.js
+++ front-end/src/pages/pageList/Profile.js
@@ -3,15 +3,9 @@ import axios from 'axios';
 import parse from 'html-react-parser';
 function Profile() {
   let [returnData, setReturnData] = useState("");
-  let URL;
-  if(process.env.NODE_ENV === 'production'){
-    URL = 'https://willkiss.ml:4000/';
-  }else{
-    URL = 'http://localhost:4000/';
-  }
   useEffect(() => {
     async function readContent() {
-      const ret = await axios.get(URL + 'Profile');
+      const ret = await axios.get('/api/profile');
         setReturnData(ret.data);
     }
     readContent();
