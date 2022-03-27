bee734450c9c29113fe2976c1d861e4b2a7a9fdf
st42597 <st42597@gmail.com>
2022-03-27 15:52:48 +0900

    fix: description

--- front-end/src/pages/pageList/Description.js
+++ front-end/src/pages/pageList/Description.js
@@ -2,10 +2,10 @@ function Description() {
   return(
     <div>
       <h1>my-first-web</h1>
-      <div>front-end : Node.js React Library</div>
-      <div>back-end : Node.js Express Framework</div>
-      <div>Cloud : AWS EC2, ELB</div>
-      <div>Client-Front(react)-Back(express)-LB(LoadBalancer)-AWS EC2</div>
+      <div>front: React</div>
+      <div>back: Express</div>
+      <div>proxy-server: Nginx</div>
+      <div>Container based MSA</div>
       <a href="https://github.com/st42597">GitHub</a>
     </div>
   )
